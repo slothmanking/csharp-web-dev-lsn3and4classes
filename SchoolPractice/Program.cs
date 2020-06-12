f66c39d40f4b812ed9017f30110d2a4254d5920b
Jed Morris <jemorris110@gmail.com>
2020-06-11 19:08:49 -0700

    added student.cd auto-implemented properties

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -7,6 +7,8 @@ namespace SchoolPractice
         public static void Main(string[] args)
         {
             // TODO: Instantiate your objects and test your exercise solutions with print statements here.
+
+            Console.WriteLine();
         }
     }
 }
