078e5e0848c4e9d756b3a9f5f61dce314fc2dea9
Jed Morris <jemorris110@gmail.com>
2020-06-12 07:33:43 -0700

    created Teacher class

--- SchoolPractice/Teacher.cs
+++ SchoolPractice/Teacher.cs
@@ -3,6 +3,12 @@ namespace SchoolPractice
 {
     public class Teacher
     {
+        private string FirstName;
+        private string LastName;
+        private string Subject;
+        private int YearsTeaching;
+
+
         public Teacher()
         {
         }
