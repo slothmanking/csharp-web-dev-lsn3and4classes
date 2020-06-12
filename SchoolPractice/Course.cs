20aa9d53d93cdef6a906c03cd9c243a47d9e3ec8
Jed Morris <jemorris110@gmail.com>
2020-06-12 07:21:41 -0700

    created course class

---
+++ SchoolPractice/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SchoolPractice
+{
+    public class Course
+    {
+        private string List<Student>(enrolledStudents);
+        private string courseTitle;
+        private string location;
+
+        public Course()
+        {
+        }
+    }
+}
