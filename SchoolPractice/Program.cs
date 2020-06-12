74dba401dfd5b4e4d445315023d865dba6ceaa36
Jed Morris <jemorris110@gmail.com>
2020-06-11 19:31:40 -0700

    created new student class Jed

--- SchoolPractice/Program.cs
+++ SchoolPractice/Program.cs
@@ -6,15 +6,9 @@ namespace SchoolPractice
     {
         public static void Main(string[] args)
         {
+            Student jed = new Student("Jed", 110, 1, 4.0);
 
-            public Student(string name, int id, int credits, double gpa)
-            {
-                Name = name;
-                StudentId = id;
-                NumberOfCredits = credits;
-                Gpa = gpa; 
 
-            }
         }
     }
 }
